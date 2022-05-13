516b7587c6101cdd6e8643b81d164bc214b27fec
Vinicius Oliveira <vicevini@live.com>
2022-05-13 17:18:03 -0300

    select task

--- projetinho-ts/src/components/lista/Item/index.tsx
+++ projetinho-ts/src/components/lista/Item/index.tsx
@@ -1,11 +1,33 @@
-import { ITarefa } from '../../../types/tarefa';
-import style from '../style.module.scss';
+import { ITarefa } from "../../../types/tarefa";
+import style from "../style.module.scss";
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={style.item}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
-  ); 
+  );
 }
