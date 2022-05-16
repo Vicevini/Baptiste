e5a08c19fbcc1757dbc22aa86ad82ce8eb5124be
Vinicius Oliveira <vicevini@live.com>
2022-05-15 23:46:39 -0300

    initializing timer

--- projetinho-ts/src/components/Cronometro/index.tsx
+++ projetinho-ts/src/components/Cronometro/index.tsx
@@ -1,15 +1,28 @@
 import Botao from "../Botão";
-import Relogio  from "./Relogio";
+import Relogio from "./Relogio";
 import style from "./cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useState } from "react";
 
-export default function Cronometro() {
-    return (
-      <div className={style.cronometro}>
-        <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-        <div className={style.relogioWrapper}>
-          <Relogio />
-        </div>
-        <Botao>Começar</Botao>
+interface Props {
+  selecionado: ITarefa | undefined;
+  finalizarTarefa: () => void;
+}
+
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
+  const [tempo, setTempo] = useState<number>();
+  if(selecionado?.tempo){
+    setTempo(tempoParaSegundos(selecionado.tempo));
+  }
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {tempo}
+      <div className={style.relogioWrapper}>
+        <Relogio />
       </div>
-    );
+      <Botao>Começar</Botao>
+    </div>
+  );
 }
--- projetinho-ts/src/pages/App.tsx
+++ projetinho-ts/src/pages/App.tsx
@@ -7,22 +7,25 @@ import styles from "./styles.module.scss";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
-  const [selecionado, setSelecionado] = useState<ITarefa | null>(null);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
 
   function selecionaTarefa(tarefaSelecionada: ITarefa) {
     setSelecionado(tarefaSelecionada);
-    setTarefas(tarefasAnteriores => tarefasAnteriores.map(
-      tarefa => ({
+    setTarefas((tarefasAnteriores) =>
+      tarefasAnteriores.map((tarefa) => ({
         ...tarefa,
-        selecionado: tarefa.id === tarefaSelecionada.id ? true : false
-      })));
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+      }))
+    );
   }
 
   return (
     <div className={styles.AppStyle}>
       <Formulario setTarefas={setTarefas} />
       <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
-      <Cronometro />
+      <Cronometro selecionado={selecionado} finalizarTarefa={function (): void {
+        throw new Error("Function not implemented.");
+      } } />
     </div>
   );
 }
