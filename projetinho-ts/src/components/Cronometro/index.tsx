79362d5451a09be82ee99c3df3aba60756d65aa7
Vinicius Oliveira <vicevini@live.com>
2022-05-27 15:02:15 -0300

    countdown

--- projetinho-ts/src/components/Cronometro/index.tsx
+++ projetinho-ts/src/components/Cronometro/index.tsx
@@ -12,19 +12,28 @@ interface Props {
 
 export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
   const [tempo, setTempo] = useState<number>();
- 
+
   useEffect(() => {
     if (selecionado) {
       setTempo(tempoParaSegundos(selecionado.tempo));
     }
   }, [selecionado]);
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+    }, 1000);
+  }
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo} />
       </div>
-      <Botao>Começar</Botao>
+      <Botao onClick={() => regressiva(tempo)}>Começar</Botao>
     </div>
   );
 }
